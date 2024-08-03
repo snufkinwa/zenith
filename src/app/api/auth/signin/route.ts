f1520c0fb268da7ea9924b79a9769afa430e2bdb
Janay Harris <jha2972@wgu.edu>
2024-08-03 13:26:21 -0500

    fixing api

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,16 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabaseServerClient } from '@/utils/supabaseServerClient';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { email, password } = req.body;
-    const { error } = await supabaseServerClient.auth.signInWithPassword({ email, password });
 
-    if (error) {
-      return res.status(401).json({ error: error.message });
+    try {
+      const { error } = await supabaseServerClient.auth.signInWithPassword({ email, password });
+
+      if (error) {
+        return res.status(401).json({ error: error.message });
+      }
+
+      res.status(200).json({ message: 'Signed in successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    res.status(200).json({ message: 'Signed in successfully' });
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
--- src/app/api/auth/signout/signout.ts
+++ src/app/api/auth/signout/signout.ts
@@ -1,15 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { supabaseServerClient } from '@/utils/supabaseServerClient';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const { error } = await supabaseServerClient.auth.signOut();
+    try {
+      const { error } = await supabaseServerClient.auth.signOut();
 
-    if (error) {
-      return res.status(401).json({ error: error.message });
+      if (error) {
+        return res.status(401).json({ error: error.message });
+      }
+
+      res.status(200).json({ message: 'Signed out successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    res.status(200).json({ message: 'Signed out successfully' });
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
