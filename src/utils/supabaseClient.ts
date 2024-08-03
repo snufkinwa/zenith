6410cd32022b3d7415cc36e5f780eeb69932abc2
Janay Harris <jha2972@wgu.edu>
2024-08-03 14:21:21 -0500

    ..

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,28 +1,6 @@
-import { type GetServerSidePropsContext } from 'next';
-import { createServerClient, serializeCookieHeader } from '@supabase/ssr';
+import { createClient } from '@supabase/supabase-js'
 
-export function createClient({ req, res }: GetServerSidePropsContext) {
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          console.log('Getting cookies:', req.cookies);
-          return Object.keys(req.cookies).map((name) => ({ name, value: req.cookies[name] || '' }));
-        },
-        setAll(cookiesToSet) {
-          console.log('Setting cookies:', cookiesToSet);
-          res.setHeader(
-            'Set-Cookie',
-            cookiesToSet.map(({ name, value, options }) =>
-              serializeCookieHeader(name, value, options)
-            )
-          );
-        },
-      },
-    }
-  );
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-  return supabase;
-}
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
